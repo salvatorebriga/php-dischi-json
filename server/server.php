9cfeec53177ec1036c4d4887fa3c81447fc8498e
salvatorebriga <salvo.z2302@gmail.com>
2024-06-23 17:41:20 +0200

    added dischi.json comunication

--- server/server.php
+++ server/server.php
@@ -1,32 +1,7 @@
 <?php
 
-$dischi = [
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-    [
-        'name' => 'Gotye',
-        'url' => '',
-    ],
-];
+$dischi = file_get_contents('./dischi.json');
 
 header('Content-Type: application/json');
 
-echo json_encode($dischi);
+echo $dischi;
