a1cab3799ce6e864af019a3db77781e1a54f0e86
salvatorebriga <salvo.z2302@gmail.com>
2024-06-23 20:08:16 +0200

    Bonus Lightbox

--- client/src/main.js
+++ client/src/main.js
@@ -6,14 +6,30 @@ createApp({
       title: "PHP DISCHI",
       url: "http://localhost/php-dischi-json/server/server.php",
       dischi: [],
+      selectedDisc: null,
     };
   },
 
   methods: {
     getDischi() {
-      axios.get(this.url).then((response) => {
-        this.dischi = response.data;
-      });
+      axios
+        .get(this.url)
+        .then((response) => {
+          this.dischi = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    },
+
+    getModal(index) {
+      this.selectedDisc = this.dischi.find((disc) => disc.id === index);
+      const modal = new bootstrap.Modal(
+        document.getElementById("staticBackdrop")
+      );
+      modal.show();
+
+      console.log(this.selectedDisc);
     },
   },
   created() {
