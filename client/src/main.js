6875d6b5f920c476b6bb0753ac30725ab3f3ef12
salvatorebriga <salvo.z2302@gmail.com>
2024-06-23 17:31:27 +0200

    axios get

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,19 @@ createApp({
   data() {
     return {
       title: "PHP DISCHI",
+      url: "http://localhost/php-dischi-json/server/server.php",
+      dischi: [],
     };
+  },
+
+  methods: {
+    getDischi() {
+      axios.get(this.url).then((response) => {
+        this.dischi = response.data;
+      });
+    },
+  },
+  created() {
+    this.getDischi();
   },
 }).mount("#app");
